32f109be48a1cbb5329ecb8528d584875ee719ff
Rebecca Padgett <110476306+bccpadge@users.noreply.github.com>
2024-01-30 13:00:39 -0500

    Update index.html and script.js
    changed files due to Frontend Mentor feedback

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,6 +57,9 @@
   );
 })();
 
+const plusIcon = `<svg class="accordion__icon add" xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg>`;
+const minusIcon = `<svg class="accordion__icon minus" xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#301534" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"/></svg>`;
+
 class Accordion {
   constructor(element, options) {
     this.acc = element;
@@ -72,8 +75,11 @@ class Accordion {
     this.buttons.forEach((btn, index) => {
       btn.id = `btn-${this.id}-${index}`;
       btn.setAttribute("aria-expanded", "false");
-      const plusIcon = `<svg class="accordion__icon add" xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg>`;
-      const minusIcon = `<svg class="accordion__icon minus" xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#301534" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"/></svg>`;
+
+      // !first expanded option
+      if (this.options.firstExpanded && index === 0) {
+        btn.setAttribute("aria-expanded", "true");
+      }
 
       btn.setAttribute("aria-controls", `panel-${this.id}-${index}`);
       btn.insertAdjacentHTML("beforeend", plusIcon);
@@ -85,6 +91,11 @@ class Accordion {
       panel.setAttribute("role", "region");
       panel.setAttribute("aria-labelledby", `btn-${this.id}-${index}`);
       panel.setAttribute("hidden", "");
+
+      // !first expanded option panel
+      if (this.options.firstExpanded && index === 0) {
+        panel.removeAttribute("hidden");
+      }
     });
   }
 
@@ -94,12 +105,17 @@ class Accordion {
       const expanded = e.target.getAttribute("aria-expanded") == "true";
       const index = this.buttons.findIndex((btn) => btn.id === e.target.id);
 
-      // !Option for one panel
+      // !open one panel
       if (this.options.onePanel) {
         this.panels.forEach((panel) => {
-          panel.setAttribute("hidden", "true");
+          panel.setAttribute("hidden", "");
         });
       }
+
+      // ! switch icons when one panel is closed
+      this.buttons.forEach((btn) => {
+        btn.setAttribute("aria-expanded", "false");
+      });
 
       if (expanded) {
         e.target.setAttribute("aria-expanded", "false");
